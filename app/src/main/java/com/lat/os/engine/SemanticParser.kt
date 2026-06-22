48258a13cadaf003ed9fbb5266cfd4d304ae1d22
MD Asad Ullah <mdasadullahmaruf1@gmail.com>
2026-06-22 19:17:12 +0400

    Update SemanticParser.kt

--- app/src/main/java/com/lat/os/engine/SemanticParser.kt
+++ app/src/main/java/com/lat/os/engine/SemanticParser.kt
@@ -1,4 +1,3 @@
-
 // com/lat/os/engine/SemanticParser.kt
 package com.lat.os.engine
 
@@ -6,13 +5,19 @@ import com.lat.os.data.Action
 
 object SemanticParser {
     private val patterns = listOf(
-        Regex("open (.+)", RegexOption.IGNORE_CASE) to { match: MatchResult -> Action("OPEN_APP", match.groupValues[1]) },
+        Regex("open (.+)", RegexOption.IGNORE_CASE) to { match: MatchResult ->
+            Action("OPEN_APP", match.groupValues[1])
+        },
         Regex("scroll down", RegexOption.IGNORE_CASE) to { Action("SCROLL_DOWN", "") },
         Regex("scroll up", RegexOption.IGNORE_CASE) to { Action("SCROLL_UP", "") },
         Regex("go back", RegexOption.IGNORE_CASE) to { Action("GO_BACK", "") },
         Regex("go home", RegexOption.IGNORE_CASE) to { Action("GO_HOME", "") },
-        Regex("click (.+)", RegexOption.IGNORE_CASE) to { match: MatchResult -> Action("TAP_TEXT", match.groupValues[1]) },
-        Regex("type (.+)", RegexOption.IGNORE_CASE) to { match: MatchResult -> Action("TYPE_TEXT", match.groupValues[1]) }
+        Regex("click (.+)", RegexOption.IGNORE_CASE) to { match: MatchResult ->
+            Action("TAP_TEXT", match.groupValues[1])
+        },
+        Regex("type (.+)", RegexOption.IGNORE_CASE) to { match: MatchResult ->
+            Action("TYPE_TEXT", match.groupValues[1])
+        }
     )
 
     fun parse(input: String): Action? {
